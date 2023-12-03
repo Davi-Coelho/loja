9d50e11745e32acb562d09aaa4420292a50bd7f2
Davi-Coelho <davi.cmbarreto@gmail.com>
2023-12-03 17:13:20 -0400

    feat: :sparkles: add: repository do usuário como provider

--- src/usuario/usuario.repository.ts
+++ src/usuario/usuario.repository.ts
@@ -1,9 +1,11 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
 export class UsuarioRepository {
   private usuarios = [];
 
   async salvar(usuario) {
     this.usuarios.push(usuario);
-    console.log(this.usuarios);
   }
 
   async listar() {
